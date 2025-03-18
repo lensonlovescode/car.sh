8883b6c2bc095fd0d90672e011cbe4ca7e33d2d7
Shem Murimi <murithi.murimi23@students.dkut.ac.ke>
2025-03-18 18:56:01 +0300

    Add blueprint for faq & howto

--- web_flask/app.py
+++ web_flask/app.py
@@ -3,12 +3,14 @@
 The main entry point of the application
 """
 from web_flask.pages import app_pages
+from web_flask.pages import web
 from flask import Flask
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "verysecretpassword"
 app.register_blueprint(app_pages)
+app.register_blueprint(web)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
--- web_flask/pages/main.py
+++ web_flask/pages/main.py
@@ -3,23 +3,13 @@
 The routes for the main page
 """
 from web_flask.pages import app_pages
+from web_flask.pages import web
 from flask import render_template
 
 
 @app_pages.route('/', strict_slashes=False, methods=["GET", "POST"])
 def main_page():
     return render_template('index.html')
-
-
-@app_pages.route('/howto', strict_slashes=False)
-def howto():
-    return render_template("howto.html")
-
-
-@app_pages.route('/faq', strict_slashes=False)
-def faq():
-    return render_template("faq.html")
-
 
 if __name__ == '__main__':
     app.run(debug=True)
