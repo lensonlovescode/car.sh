cd5d55fa993ce5112d78ebc33a40cbe8d3c320cc
Shem Murimi <murithi.murimi23@students.dkut.ac.ke>
2025-03-20 11:08:55 +0300

    Fix overlay

--- web_flask/app.py
+++ web_flask/app.py
@@ -16,4 +16,4 @@ app.register_blueprint(web)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
--- web_flask/pages/main.py
+++ web_flask/pages/main.py
@@ -12,4 +12,4 @@ def main_page():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
